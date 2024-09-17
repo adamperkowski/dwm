f30fbfdc8c587ff52ec830138f75ba1282642f8c
Adam Perkowski <adas1per@protonmail.com>
2024-09-18 01:32:31 +0200

    Added `dragmfact` & removed layout symbols

--- config.def.h
+++ config.def.h
@@ -38,9 +38,9 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
 	/* symbol   arrange function */
-	{ "󰬛",      tile },    /* first entry is default */
-	{ "󰬍",      NULL },    /* no layout function means floating behavior */
-	{ "󰬔",      monocle },
+	{ "",      tile },    /* first entry is default */
+	{ "",      NULL },    /* no layout function means floating behavior */
+	{ "",      monocle },
 };
 
 /* key definitions */
