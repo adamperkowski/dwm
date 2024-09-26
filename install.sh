9a31427a9bc80518347c9365ca679ac87b984855
Adam Perkowski <adas1per@protonmail.com>
2024-09-26 13:14:26 +0200

    zoxide

--- install.sh
+++ install.sh
@@ -41,7 +41,7 @@ fi
 printf "%b\n" "${YELLOW}Installing dependencies...${RC}"
 $SU pacman -S --needed --noconfirm base-devel fastfetch zsh xorg xorg-xinit xorg-xsetroot ttf-firacode-nerd pipewire \
     p7zip noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-nerd-fonts-symbols kitty rofi flameshot zsh zsh-syntax-highlighting \
-    zsh-autosuggestions hsetroot > /dev/null 2>&1 || { printf "%b\n" "${RED}Failed to install dependencies.${RC}"; exit 1; }
+    zsh-autosuggestions hsetroot zoxide > /dev/null 2>&1 || { printf "%b\n" "${RED}Failed to install dependencies.${RC}"; exit 1; }
 $AUR_HELPER -S --needed --noconfirm picom-ftlabs-git lemurs > /dev/null 2>&1 || { printf "%b\n" "${RED}Failed to install AUR dependencies.${RC}"; exit 1; }
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended > /dev/null 2>&1 || printf "%b\n" "${RED}Failed to install Oh My ZSH. It might be already installed.${RC}"
 printf "%b\n" "${GREEN}Dependencies installed.${RC}"
